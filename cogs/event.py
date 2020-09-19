dd6731c994c52575b2bd5c1ee97bece0f323ff18
TheMisterSenpai <61903983+TheMisterSenpai@users.noreply.github.com>
2020-09-19 19:45:32 +0300

    обновление 2.5

--- cogs/event.py
+++ cogs/event.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from discord.utils import get
 
 import config
-from Utils import DB
 
 class event(commands.Cog):
 
@@ -13,62 +12,21 @@ class event(commands.Cog):
         self.cog_name = ["Ивенты", True]
     #Зачем ты зашел в этот файл, он так для красоты
 
-    @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-        Get = DB.Get()
-        Set = DB.Set()
-        if after.channel:
-            channel = Get.options("channels")[0]
-            category = Get.options("category")[0]
+    @commands.Cog.listener() 
+    async def on_member_message(self, member):
+        await client.process_commands( message )
 
-            if int(after.channel.id) == int(channel):
-                cat = discord.utils.get(member.guild.categories, id=int(category))
-                channel2 = await member.guild.create_voice_channel(name=f"{member.name}#{member.discriminator}",
-                                                                   category=cat)
-                await member.move_to(channel2)
-                await channel2.set_permissions(member, manage_channels=True)
-                Set.privateChannels(channel2, member)
+	    if not message.author.bot:
+		    with open('./Data/DataBase/economy.json',"r") as f:
+			    users = json.load(f)
 
-            elif before.channel:
-                if str(before.channel.id) in str(Get.privateChannels(member)[0]):
-                    try:
-                        await before.channel.delete()
-                    except Exception:
-                        pass
+        async def update_money(users, user):
+            if not user in users:
+                users[user] = {}
+                users[user]['money'] = 300    
 
-        else:
-            if before.channel:
-                if str(before.channel.id) in str(Get.privateChannels(member)[0]):
-                    try:
-                        await before.channel.delete()
-                    except Exception:
-                        pass
-
-
-    @commands.Cog.listener() #Дает 0 варнов кто присоединился
-    async def on_member_join(self, member):
-        
-        conn = sqlite3.connect('./Data/DataBase/warn_users.db')
-        cursor = conn.cursor()
-
-        if cursor.execute(f"SELECT id FROM users WHERE id = {member.id}").fetchone() is None:
-            cursor.execute(f"INSERT INTO users VALUES('{member.id}', 0)")
-            conn.commit()
-        else:
-            pass
-
-    @commands.Cog.listener() #Убирает человека который вышел с сервера
-    async def on_member_remove(self, member):
-
-        conn = sqlite3.connect('./Data/DataBase/warn_users.db')
-        cursor = conn.cursor()
-
-        if cursor.execute(f"SELECT id FROM users WHERE id = {member.id}").fetchone() is None:
-            cursor.execute(f"DELETE FROM users WHERE id = {member.id}")
-            conn.commit()
-        else:
-            pass    
-                  
+		with open('./Data/DataBase/economy.json',"w") as f:
+			json.dump(users,f)            
    
 def setup(client):
     client.add_cog(event(client))                
---
+++ console.bat
@@ -0,0 +1,3 @@
+@echo off
+start cmd
+cd E:\discordBOT 2.0\catdiva2.0
